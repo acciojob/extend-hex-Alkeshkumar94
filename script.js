9067496d56ff6fc960f92b4de1d75af991f66f07
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-10 14:16:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i=shortHex.length-3,iscapital=false,output="#";
+	while(i<shortHex.length){
+	let asciivalue=shortHex[i].chartAt(0);
+		if(asciivalue>=65 && asciivalue<=90)
+		{
+			iscapital=true;
+		}
+		output+=shortHex[i]+shortHex[i];
+		i++;
+	}
+	return output?output.toUpperCase():output;
 };
 
 // Do not change the code below.
